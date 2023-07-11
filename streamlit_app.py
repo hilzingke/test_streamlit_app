01cf60ce880e1c94aa31ab7daf157756eb911b7a
hilzingke <72766611+hilzingke@users.noreply.github.com>
2023-07-11 15:02:06 +0200

    Update streamlit_app.py
    added menu header and items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('Diner Demo')
 
-streamlit.text('this is an example text')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
